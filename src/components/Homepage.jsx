236b1bceeaac2211375107b364731705cf48c74c
Chris Shockley <cishockley3@gmail.com>
2021-10-27 13:55:29 -0400

    added cryptoApi, connected to Homepage through Redux

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,17 +2,17 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
-// import { useGetCryptosQuery } from '../services/cryptoApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Cryptocurrencies, News } from '../components/';
-import Loader from './Loader';
+// import Loader from './Loader';
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  //   const { data, isFetching } = useGetCryptosQuery(10);
-  //   const globalStats = data?.data?.stats;
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
 
-  if (isFetching) return <Loader />;
+  if (isFetching) return 'Loading...';
 
   console.log(data);
   return (
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'ab8ec6c1eemsh98fb82dd86ceffep11acb8jsnee796cf5a684',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getExchanges: builder.query({
+      query: () => createRequest(`/exchanges`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coindId) => createRequest(`/coin/${coindId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timePeriod }) =>
+        createRequest(`/coin/${coinId}/history/${timePeriod}`),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+  useGetExchangesQuery,
+} = cryptoApi;
